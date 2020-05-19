b54a5b4999a8903c577e757da6c2a91dab13027f
Deepak Prasad <deepak140596@gmail.com>
2020-05-20 00:42:21 +0530

    Birth of Rakshak

---
+++ app/src/main/java/com/dpk/rakshak/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.dpk.rakshak
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
